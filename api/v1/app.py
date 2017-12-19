5ce08b7371b4bd93f0c29cb40f98de23c1b666b2
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-12-19 19:26:31 +0000

    task 6: error handler added

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,15 @@
 app = Flask('app_views')
 from models import storage
 from api.v1.views import app_views
+from flask import jsonify
 
 @app.teardown_appcontext
 def close():
     return storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify(error='Not found'), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
