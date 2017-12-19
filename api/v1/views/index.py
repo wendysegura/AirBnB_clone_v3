37b2b87cdc84e7b0381d60dcfa85b35d541a55bc
wendysegura <118@holbertonschool.com>
2017-12-19 17:21:29 +0000

    upload

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,4 +14,17 @@ def response_status():
 @app_views.route('/api/v1/stats')
 def get_count():
     """"retrieves the number of each objects by type"""
+    count = {}
     
+    name2class = {
+    'Amenity': Amenity,
+    'City': City,
+    'Place': Place,
+    'State': State,
+    'Review': Review,
+    'User': User
+}
+    for cls in name2class:
+        number = storage.count(cls)
+        count[name2class.get(cls)] = number
+    return jsonify(count)
