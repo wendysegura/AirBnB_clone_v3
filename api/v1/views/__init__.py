0ef50a8ba88bcfc050505776e41ec9326f670bb7
wendysegura <118@holbertonschool.com>
2017-12-20 17:59:55 +0000

    took off import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,15 +6,3 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *   # noqa
 from api.v1.views.states import *  # noqa
-from api.v1.views.cities import *  # noqa
-from api.v1.views.users import *  # noqa
-from api.v1.views.places import *  # noqa
-from api.v1.views.amenities import *  # noqa
-from models import storage
-from models.state import State
-from models.city import City
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
