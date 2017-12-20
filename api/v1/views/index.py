925c937889084f9013486d6565e691571e9f4774
wendysegura <118@holbertonschool.com>
2017-12-20 16:58:08 +0000

    fixed pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,21 +8,21 @@ from models import storage
 @app_views.route('/status')
 def response_status():
     """return json on object appviews"""
-    response = jsonify({'status':'OK'}), 200
+    response = jsonify({'status': 'OK'}), 200
     return response
+
 
 @app_views.route('/stats')
 def get_count():
     """"retrieves the number of each objects by type"""
-    obj= {}
-    name2class = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+    obj = {}
+    name2class = {"amenities": "Amenity",
+                  "cities": "City",
+                  "places": "Place",
+                  "reviews": "Review",
+                  "states": "State",
+                  "users": "User"}
+
     for cls in name2class:
         number = storage.count(obj)
         obj[name2class.get(cls)] = number
