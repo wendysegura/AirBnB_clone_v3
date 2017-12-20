17df689b61c786c8935624902604b9d2bbd70434
wendysegura <118@holbertonschool.com>
2017-12-20 17:08:13 +0000

    fixed host port

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,7 +18,9 @@ def close(self):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return jsonify(error='Not found'), 404
+    return jsonify({error='Not found'}, 404)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    host_1 = getenv('HBNB_API_HOST', '0.0.0.0')
+    port_1 = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host_1, port=port_1)
