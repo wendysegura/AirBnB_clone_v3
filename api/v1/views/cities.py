8b490fcefccb2caf7e45616bd3b63aa1c8cb5d9f
wendysegura <118@holbertonschool.com>
2017-12-20 01:32:03 +0000

    upload

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -12,25 +12,43 @@ from models import storage
 def all_cities(state_id):
     """Retrieves the list of all city objects"""
     state = storage.get("State", state_id)
+    list_cities = []
+
+    if not state:
+        abort(404)
+    for city in state.cities:
+        if city.state_id == state_id:
+            list_cities.append(city.to_dict())
+    return jsonify(list_cities)
     
-
-
 @app_views.route('/cities/city_id', methods=['GET'])
 def get_city(city_id):
     """Retrieves the State object"""
-
+    city = storage.get("City", city_id)
+    if not city:
+        abort(404)
+    return jsonify(city.to_dict())
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
     """Deletes an object"""
-
+    city = storage.get("City", city_id)
+    if not city:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return jsonify({}), 200
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def create_city(state_id):
     """Creates a City"""
-
+    state = storage.get("State", state_id)
+    if not city:
+        abort(404)
+    
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
 def update_city(city_id):
     """Updates a city object"""
-    
+    if not city:
+        abort(404)
