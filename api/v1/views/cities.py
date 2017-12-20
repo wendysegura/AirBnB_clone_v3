3e23e9fbe12b7d67c51d465cdc2cbcf8448fe0a0
wendysegura <118@holbertonschool.com>
2017-12-20 18:11:26 +0000

    fixed pep8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,8 @@ from models.city import City
 from models import storage
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def all_cities(state_id):
     """Retrieves the list of all city objects"""
     state = storage.get("State", state_id)
@@ -20,7 +21,8 @@ def all_cities(state_id):
         if city.state_id == state_id:
             list_cities.append(city.to_dict())
     return jsonify(list_cities)
-    
+
+
 @app_views.route('/cities/city_id', methods=['GET'])
 def get_city(city_id):
     """Retrieves the State object"""
@@ -28,6 +30,7 @@ def get_city(city_id):
     if not city:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
@@ -39,7 +42,9 @@ def delete_city(city_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """Creates a City"""
     state = storage.get("State", state_id)
@@ -50,9 +55,10 @@ def create_city(state_id):
         abort(404, "Not a JSON")
     if 'name' not in body_dict:
         abort(404, "Missing name")
-    
+
     city.save()
     return jsonify(city_to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
 def update_city(city_id):
@@ -64,7 +70,7 @@ def update_city(city_id):
     body_dict = request.get_json
     if not dict_body:
         abort(404, "Not a JSON")
-    
+
     ignore_keys = ["id", "state_id", "created_at", "updated_at"]
     if city:
         for key, value in dict_body.items():
