b426c521046250e701e2fd991c3abd3966b2b282
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-12-20 21:18:11 +0000

    task 7: getting error code 404

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,39 +1,59 @@
 #!/usr/bin/python3
-"""new view for State objects"""
-
-
-from flask import jsonify, 
-from api.v1.views import app_views
-from models.state import State
+"""creates a new view for State objects"""
 from models import storage
+from models.state import State
+from api.v1.views import app_views
+from flask import Flask, abort, request, jsonify
 
 
-@app_views.route('/states',     )
-def all_states():
-    """Retrieves the list of all State objects"""
-    """GET /api/v1/states"""
-
-
-
-@app_views.route('/states',     )
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_state():
-    """Retrieves the State object"""
-    """GET /api/v1/states/<state_id>"""
+    """ retrieves the list of all State obj """
+    all_state = []
+    states = storage.all('State').values()
+    for s in states:
+        all_state.append(s.to_dict())
+    return jsonify(all_state)
 
 
-@app_views.route('/states',     )
-def delete_state():
-    """Deletes a State object"""
-    """DELETE /api/v1/states/<state_id>"""
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_id(state_id):
+    """ retrieves State object """
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
 
 
-@app_views.route('/states',     )
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """ delete State object if no id """
+    state = storage.get("State", state_id)
+    empty = {}
+    if state is None:
+        abort(404)
+    else:
+        storage.delete(state)
+    return (jsonify(empty), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
-    """Creates a State"""
-    """POST /api/v1/states"""
+    """ creates State object """
+    req = request.get_json()
+    if req is None:
+        return (jsonify("Not a JSON"), 400)
+    elif "name" not in req:
+        return (("Missing name"), 400)
+    state = State(**req)
+    state.save()
+    return (jsonify(state.to_dict()), 201)
 
 
-@app_views.route('/states',     )
-def update_state():
-    """Updates a State object"""
-    """PUT /api/v1/states/<state_id>"""
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    state = storage.get("State", state_id)
+    req = request.get_json()
+    if req is None:
+        return (jsonify("Not a JSON"), 400)
