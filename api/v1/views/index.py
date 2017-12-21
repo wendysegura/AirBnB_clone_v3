efb236ae81925c2a8dc3ec5165f3323e409951fe
wendysegura <118@holbertonschool.com>
2017-12-21 12:16:56 -0800

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,23 +3,16 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
-from models.state import State
-from models.city import City
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
-@app_views.route('/status/', strict_slashes=False)
+@app_views.route('/status/')
 def response_status():
     """return json on object appviews"""
-    response = jsonify({'status': 'OK'})
+    response = jsonify({"status": "OK"})
     return response
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route('/stats/')
 def get_count():
     """"retrieves the number of each objects by type"""
     obj = {}
