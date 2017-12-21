6680c5d11b32232fadbdc0d2156c686f4e8e420b
wendysegura <118@holbertonschool.com>
2017-12-21 15:08:36 -0800

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,7 +4,6 @@ from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models.city import City
 from models import storage
-from models.state import State
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
