804aa1ced300f732b3a38e66bcc58734465ec2ff
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-12-21 03:42:27 +0000

    task 13: added cors statement and imported cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,12 @@ from os import getenv
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, Response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close(self):
