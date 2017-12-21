4d6e59ebb844c9d1f8d2d1265367c8a1f6f8114c
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-12-21 03:43:52 +0000

    app.py: fixed pep8 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def close(self):
     storage.close()
