64603f9732bec28089b328514437bc3ccf050bce
wendysegura <118@holbertonschool.com>
2017-12-21 19:20:12 +0000

    added doc strings

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,20 +7,20 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, Response
 from flask_cors import CORS
-
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def close(self):
+    """teardown"""
     storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """error"""
     return jsonify(error='Not found'), 404
 
 if __name__ == "__main__":
